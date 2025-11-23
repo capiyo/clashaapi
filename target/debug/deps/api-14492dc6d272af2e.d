96e85ab520649815c4946c7ce5c3f8e38ad7fe1e
Capiyo <engineercapiyo@gmail.com>
2025-11-23 15:55:46 +0300

    update

--- target/debug/deps/api-14492dc6d272af2e.d
+++ target/debug/deps/api-14492dc6d272af2e.d
@@ -1,6 +1,6 @@
-D:\RUSt\clash\api\target\debug\deps\api-14492dc6d272af2e.d: src\main.rs src\routes\mod.rs src\routes\auth.rs src\routes\games.rs src\routes\posts.rs src\models\mod.rs src\models\user.rs src\models\game.rs src\models\post.rs src\handlers\mod.rs src\handlers\auth.rs src\handlers\games.rs src\handlers\posts.rs src\handlers\upload.rs src\middleware\mod.rs src\middleware\auth.rs src\database\mod.rs src\database\connection.rs src\errors.rs
+D:\RUSt\clash\api\target\debug\deps\api-14492dc6d272af2e.d: src\main.rs src\routes\mod.rs src\routes\auth.rs src\routes\games.rs src\routes\posts.rs src\models\mod.rs src\models\user.rs src\models\game.rs src\models\post.rs src\handlers\mod.rs src\handlers\auth.rs src\handlers\games.rs src\handlers\posts.rs src\handlers\upload.rs src\handlers\kalka.rs src\middleware\mod.rs src\middleware\auth.rs src\database\mod.rs src\database\connection.rs src\errors.rs
 
-D:\RUSt\clash\api\target\debug\deps\libapi-14492dc6d272af2e.rmeta: src\main.rs src\routes\mod.rs src\routes\auth.rs src\routes\games.rs src\routes\posts.rs src\models\mod.rs src\models\user.rs src\models\game.rs src\models\post.rs src\handlers\mod.rs src\handlers\auth.rs src\handlers\games.rs src\handlers\posts.rs src\handlers\upload.rs src\middleware\mod.rs src\middleware\auth.rs src\database\mod.rs src\database\connection.rs src\errors.rs
+D:\RUSt\clash\api\target\debug\deps\libapi-14492dc6d272af2e.rmeta: src\main.rs src\routes\mod.rs src\routes\auth.rs src\routes\games.rs src\routes\posts.rs src\models\mod.rs src\models\user.rs src\models\game.rs src\models\post.rs src\handlers\mod.rs src\handlers\auth.rs src\handlers\games.rs src\handlers\posts.rs src\handlers\upload.rs src\handlers\kalka.rs src\middleware\mod.rs src\middleware\auth.rs src\database\mod.rs src\database\connection.rs src\errors.rs
 
 src\main.rs:
 src\routes\mod.rs:
@@ -16,6 +16,7 @@ src\handlers\auth.rs:
 src\handlers\games.rs:
 src\handlers\posts.rs:
 src\handlers\upload.rs:
+src\handlers\kalka.rs:
 src\middleware\mod.rs:
 src\middleware\auth.rs:
 src\database\mod.rs:
